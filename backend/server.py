6057515f2ae63151728677edd41ce4fe3c8b6a9f
Avinash-Tewari <avinashtewari03@gmail.com>
2025-08-25 19:19:38 +0530

    updated server.py

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,51 @@
+# # server.py
+# import os
+
+# from livekit import api
+# from flask import Flask,request
+# from flask_cors import CORS 
+# from dotenv import load_dotenv
+# from livekit.api import LiveKitAPI,ListRoomsRequest
+# import uuid
+
+# load_dotenv()
+
+# app = Flask(__name__)
+# CORS(app,resources={r"/*": {"origins": "*" }}) # so frontend can connect from a different port
+
+# async def generate_room_name():
+#    name= "room-" +str(uuid.uuid4())[:8]
+#    rooms= await get_rooms()
+#    while name in rooms:
+#       name = "room-" +str(uuid.uuid4())[:8]
+#    return name
+
+# async def get_rooms():
+#    api= LiveKitAPI()
+#    rooms= await api.room.list_rooms(ListRoomsRequest())
+#    await api.aclose()
+#    return [room.name for room in rooms.rooms]
+
+# @app.route('/getToken')
+# async def getToken():
+#   name= request.args.get("name", "my name")
+#   room= request.args.get("room", None)
+
+#   if not room:
+#      room= await generate_room_name()
+
+#   token = api.AccessToken(os.getenv('LIVEKIT_API_KEY'), os.getenv('LIVEKIT_API_SECRET')) \
+#     .with_identity(name) \
+#     .with_name(name) \
+#     .with_grants(api.VideoGrants(
+#         room_join=True,
+#         room=room,
+#     ))
+#   return token.to_jwt()
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5001, debug=True)
+
 # server.py
 import os
 
@@ -26,6 +74,11 @@ async def get_rooms():
    await api.aclose()
    return [room.name for room in rooms.rooms]
 
+@app.route("/")
+def home():
+    return {"status": "ok", "message": "Backend running!"}
+
+
 @app.route('/getToken')
 async def getToken():
   name= request.args.get("name", "my name")
@@ -44,5 +97,5 @@ async def getToken():
   return token.to_jwt()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
-
+    port = int(os.environ.get("PORT", 5001))  
+    app.run(host="0.0.0.0", port=port)
